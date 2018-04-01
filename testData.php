3b2dde584ac98d513e57e5e8afabd069b8fadf90
Austin Kerr <akerr@iastate.edu>
2018-03-31 22:01:13 -0500

    Update and rename testData.txt to testData.php

--- testData.php
+++ testData.php
@@ -1,7 +1,9 @@
-{"DoorEvents":
+<?php
+ech '{"DoorEvents":
     ["id":0, "start":"5pm", "duration":"5min"],
     ["id":0, "start":"6pm", "duration":"6min"],
     ["id":0, "start":"7pm", "duration":"7min"],
     ["id":0, "start":"8pm", "duration":"8min"],
     ["id":0, "start":"9pm", "duration":"9min"]
-}
+}'
+?>
